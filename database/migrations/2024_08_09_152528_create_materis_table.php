d02faa8f7241d142885e6d7cbd73d8ecd3a98d2c
Mohammad Ricko Aprilianto <ricko.aprilianto00@gmail.com>
2024-08-10 16:39:35 +0800

    init

---
+++ database/migrations/2024_08_09_152528_create_materis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('materis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignid('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->string('judul');
+            $table->text('deskripsi');
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('materis');
+    }
+};
