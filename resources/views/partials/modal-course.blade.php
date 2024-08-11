f74c850e5beeda7de6c981d88933b1baba1c18ed
Mohammad Ricko Aprilianto <ricko.aprilianto00@gmail.com>
2024-08-11 12:35:58 +0800

    update

--- resources/views/partials/modal-course.blade.php
+++ resources/views/partials/modal-course.blade.php
@@ -24,7 +24,7 @@
                         <input type="text" name="judul" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Judul" required="">
                     </div>
                     <div class="col-span-2">
-                        <label for="deskripsi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
+                        <label for="deskripsi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi</label>
                         <textarea id="deskripsi" name="deskripsi" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="deskripsi"></textarea>                    
                     </div>
                     <div class="col-span-2">
